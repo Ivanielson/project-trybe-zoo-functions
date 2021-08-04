621daf818cf0a6d0c504a030aa7d1c7e273c87a6
Ivanielson Cabral <ivanielson2011@gmail.com>
2021-08-04 19:33:27 -0300

    Requisito 6

--- src/zoo.js
+++ src/zoo.js
@@ -40,8 +40,15 @@ function isManager(id) {
   return employees.some(({ managers }) => managers.indexOf(id) !== -1);
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
   // seu código aqui
+  employees.push({
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor,
+  });
 }
 
 function countAnimals(specie) {
